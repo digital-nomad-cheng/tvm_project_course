2e3b77eebdcb112d00f81a1c18c1447daa32a4e1
digital-nomad-cheng <vincentcheng929@gmail.com>
2023-05-08 13:45:58 +0000

    Swap the order of tvm and pytorch import.

--- mnv2_auto_schedule.py
+++ mnv2_auto_schedule.py
@@ -1,8 +1,9 @@
-import tvm
-from tvm import relay
 
 import torch
 import torchvision
+
+import tvm
+from tvm import relay
 
 # load pretrained pytorch model
 def load_pretrained_model(model_name="mobilenet_v2"):
@@ -20,16 +21,17 @@ def import_pytorch_to_relay(scripted_model, input_name:str="input_tensor", input
     mod, params = relay.frontend.from_pytorch(scripted_model, shape_list)
     return mod, params
 
-def build_relay_graph(mod, params, target:str="llvm"):
-    target = tvm.target.Target(target, host="llvm")
-    dev = tvm.cpu(0)
+def build_relay_graph(mod, params, target:str="cuda"):
+    target = tvm.target.Target(target)
+    # dev = tvm.cuda(0)
+    # dev = tvm.device(str(target), 0)
     with tvm.transform.PassContext(opt_level=3):
         lib = relay.build(mod, target=target, params=params)
 
 scripted_model = load_pretrained_model()
 
-print(scripted_model)
+# print(scripted_model)
 
 mod, params = import_pytorch_to_relay(scripted_model)
 
-build_relay_graph(mod, params)
+build_relay_graph(mod, params, "nvidia/geforce-rtx-3070")
