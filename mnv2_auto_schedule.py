815185727b2d264f289a9735d7a42009d9faff44
digital-nomad-cheng <vincentcheng929@gmail.com>
2023-05-08 13:56:26 +0200

    Minor update.

--- mnv2_auto_schedule.py
+++ mnv2_auto_schedule.py
@@ -8,14 +8,28 @@ import torchvision
 def load_pretrained_model(model_name="mobilenet_v2"):
     model = getattr(torchvision.models, model_name)(pretrained=True)
     model = model.eval()
-    
+
     input_shape = [1, 3, 224, 224]
     input_tensor = torch.randn(input_shape)
     scripted_model = torch.jit.trace(model, input_tensor).eval()
     return scripted_model
 
+def import_pytorch_to_relay(scripted_model, input_name:str="input_tensor", input_shape=[1, 3, 224, 224]):
 
-model_list = torchvision.models.list_models(module=torchvision.models)
-print(model_list)
-load_pretrained_model()
+    shape_list = [(input_name, input_shape)]
+    mod, params = relay.frontend.from_pytorch(scripted_model, shape_list)
+    return mod, params
 
+def build_relay_graph(mod, params, target:str="llvm"):
+    target = tvm.target.Target(target, host="llvm")
+    dev = tvm.cpu(0)
+    with tvm.transform.PassContext(opt_level=3):
+        lib = relay.build(mod, target=target, params=params)
+
+scripted_model = load_pretrained_model()
+
+print(scripted_model)
+
+mod, params = import_pytorch_to_relay(scripted_model)
+
+build_relay_graph(mod, params)
