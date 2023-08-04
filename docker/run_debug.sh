524688a3ecb6a241c578e2c1454c8e515a5c67ec
digital-nomad-cheng <vincentcheng929@gmail.com>
2023-08-04 22:40:25 +0200

    Backup docker launch script.

--- docker/run_debug.sh
+++ docker/run_debug.sh
@@ -2,6 +2,6 @@
 # docker run -it --gpus all apache/tvm:v0.12_cuda
 
 # ...
-sudo docker run -it --gpus all --cap-add=SYS_ADMIN \
+sudo docker run -it --gpus all --cap-add=SYS_ADMIN --privileged \
 	-v /home/vincent/Work/tvm_project_course/docker/mnt:/home/work/tvm_project_course/relay/mnt \
-	apache/tvm:v0.12.0
+	apache/tvm:v0.13.0_debug
