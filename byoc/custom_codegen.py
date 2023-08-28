1776b86a6d588427bc55e9e0d0d769095f6e2e64
digital-nomad-cheng <vincentcheng929@gmail.com>
2023-08-28 16:57:18 +0000

    Add demo for using custom BYOC.

---
+++ byoc/custom_codegen.py
@@ -0,0 +1,15 @@
+import onnx
+import tvm
+from tvm import relay
+from tvm.relay import transform, build
+
+simple_model = onnx.load("simple_model.onnx")
+mod, params = tvm.relay.frontend.from_onnx(simple_model)
+
+mod["main"] = relay.build_module.bind_params_by_name(mod["main"], params)
+mod = relay.transform.AnnotateTarget(["tvmvon23"])(mod)
+mod = relay.transform.MergeCompilerRegions()(mod)
+mod = relay.transform.PartitionGraph()(mod)
+print("begin to export library...")
+with tvm.transform.PassContext(opt_level=3):
+    mod = relay.build(mod, target="llvm")
