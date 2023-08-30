d6c66575e808b5f5601f10a5dbfaf3e220881d68
digital-nomad-cheng <vincentcheng929@gmail.com>
2023-08-30 19:53:19 +0000

    Update.

--- byoc/relay_offload_acl.py
+++ byoc/relay_offload_acl.py
@@ -1,25 +1,34 @@
 import tvm
 from tvm import relay
+import onnx
 
-data_type = "float32"
-data_shape = (1, 14, 14, 512)
-strides = (2, 2)
-padding = (0, 0, 0, 0)
-pool_size = (2, 2)
-layout = "NHWC"
-output_shape = (1, 7, 7, 512)
+model_name = "simple"
+if model_name == "max_pool2d":
+    data_type = "float32"
+    data_shape = (1, 14, 14, 512)
+    strides = (2, 2)
+    padding = (0, 0, 0, 0)
+    pool_size = (2, 2)
+    layout = "NHWC"
+    output_shape = (1, 7, 7, 512)
 
-data = relay.var('data', shape=data_shape, dtype=data_type)
-out = relay.nn.max_pool2d(data, pool_size=pool_size, strides=strides, layout=layout, padding=padding)
-module = tvm.IRModule.from_expr(out)
+    data = relay.var('data', shape=data_shape, dtype=data_type)
+    out = relay.nn.max_pool2d(data, pool_size=pool_size, strides=strides, layout=layout, padding=padding)
+    module = tvm.IRModule.from_expr(out)
 
-from tvm.relay.op.contrib.arm_compute_lib import partition_for_arm_compute_lib
-module = partition_for_arm_compute_lib(module)
+    from tvm.relay.op.contrib.arm_compute_lib import partition_for_arm_compute_lib
+    module = partition_for_arm_compute_lib(module)
 
-target = "llvm -mtriple=aarch64-linux-gnu -mattr=+neon"
-with tvm.transform.PassContext(opt_level=3, disabled_pass=["AlterOpLayout"]):
-    lib = relay.build(module, target=target)
+    target = "llvm -mtriple=aarch64-linux-gnu -mattr=+neon"
+    with tvm.transform.PassContext(opt_level=3, disabled_pass=["AlterOpLayout"]):
+        lib = relay.build(module, target=target)
 
-lib_path = './lib_acl.so'
-cross_compile = 'aarch64-linux-gnu-g++'
-lib.export_library(lib_path, cc=cross_compile)
+    lib_path = './lib_acl.so'
+    cross_compile = 'aarch64-linux-gnu-g++'
+    lib.export_library(lib_path, cc=cross_compile)
+elif model_name == "simple":
+    onnx_model = onnx.load("/home/work/tvm_project_course/byoc/simple_model.onnx")
+    mod, params = relay.frontend.from_onnx(onnx_model)
+    mod.show()
+    from tvm.relay.op.contrib.arm_compute_lib import partition_for_arm_compute_lib
+    mod = partition_for_arm_compute_lib(mod)
