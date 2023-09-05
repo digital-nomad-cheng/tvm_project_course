39eec829c8f4c959cc40e033e74cd136acd1170a
digital-nomad-cheng <vincentcheng929@gmail.com>
2023-09-05 11:54:25 +0000

    Update.

--- byoc/ncnn_codegen.py
+++ byoc/ncnn_codegen.py
@@ -24,12 +24,19 @@ print("begin to export library...")
 target="llvm"
 with tvm.transform.PassContext(opt_level=3):
     lib = relay.build(mod, target=target, params=params)
+lib.export_library('simple_model_ncnn_codegen.so')
 
+print("Load library from file...")
+lib = tvm.runtime.load_module("simple_model_ncnn_codegen.so")
+
+print("Run inference with model...")
 input_tensor = np.random.rand(1, 3, 64, 64)
+print("Build graph executor...")
 m = graph_executor.GraphModule(lib["default"](tvm.cpu()))
+print("Set input...")
 m.set_input("input", tvm.nd.array(input_tensor.astype("float32")))
+print("Run module...")
 m.run()
 output = m.get_output(0)
 print(output)
 print(output.numpy().shape)
-lib.export_library('simple_model_ncnn_codegen.so')
