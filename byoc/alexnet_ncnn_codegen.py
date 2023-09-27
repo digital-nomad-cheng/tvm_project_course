6fd1432e0b01d65a88fc538acb564c4caf172dd3
digital-nomad-cheng <vincentcheng929@gmail.com>
2023-09-27 13:15:18 +0000

    Add util function to dump information to json file.

--- byoc/alexnet_ncnn_codegen.py
+++ byoc/alexnet_ncnn_codegen.py
@@ -1,7 +1,9 @@
 import time 
+import json
 
 import numpy as np
 import torch
+from torch import nn
 import torchvision
 from torchvision.models import MobileNet_V2_Weights, AlexNet_Weights
 
@@ -14,17 +16,24 @@ from tvm.relay.op.contrib.ncnn import partition_for_ncnn
 from tvm.relay.op.contrib.arm_compute_lib import partition_for_arm_compute_lib
 from tvm.relay.build_module import bind_params_by_name
 
+from alexnet import AlexNet 
+
+def extract_byoc_modules(module, codegen="ncnn"):
+    """Get the BYOC module(s) from llvm module."""
+    print("Extract BYOC modules from runtime...")
+    return list(filter(lambda mod: mod.type_key == codegen,
+                       module.get_lib().imported_modules))
+
 # load pretrained pytorch model
 def load_pretrained_model(model_name="alexnet"):
     model = getattr(torchvision.models, model_name)(weights=AlexNet_Weights.DEFAULT)
-    model = model.eval()
-
-    input_shape = [1, 3, 224, 224]
+    model.eval()
+    input_shape = [1, 3, 227, 227]
     input_tensor = torch.randn(input_shape)
     scripted_model = torch.jit.trace(model, input_tensor).eval()
     return scripted_model
 
-def import_pytorch_to_relay(scripted_model, input_name:str="input_tensor", input_shape=[1, 3, 224, 224], preprocess=True):
+def import_pytorch_to_relay(scripted_model, input_name:str="input_tensor", input_shape=[1, 3, 227, 227], preprocess=True):
 
     shape_list = [(input_name, input_shape)]
     mod, params = relay.frontend.from_pytorch(scripted_model, shape_list)
@@ -49,7 +58,7 @@ def build_mnv2_lib(mod, params, codegen="default", target="llvm"):
     
     mod.show()
 
-    with tvm.transform.PassContext(opt_level=3, disabled_pass=["AlterOpLayout"]):
+    with tvm.transform.PassContext(opt_level=0, disabled_pass=["AlterOpLayout"]):
         lib = relay.build(mod, target=target, params=params)
 
     return lib
@@ -62,16 +71,33 @@ def run(lib, numpy_input_tensor, codegen="default"):
     output = rt_mod.get_output(0)
     return output
 
+def dump_module_to_json(modules):
+    print("Dump json to files...")
+    for mod in modules:
+        source = mod.get_source("json")
+        codegen = json.loads(source)["nodes"]
+        codegen_str = json.dumps(codegen, sort_keys=True, indent=2)
+        with open('./tmp/ncnn_modules_readable.json', 'w') as outfile:
+            outfile.write(json.dumps(codegen, sort_keys=True, indent=2))
+        with open('./tmp/ncnn_modules.json', 'w') as outfile:
+            json.dump(codegen, outfile)
+
 if __name__ == "__main__":
-    numpy_input_tensor = np.random.rand(1, 3, 224, 224)
+    numpy_input_tensor = np.random.rand(1, 3, 227, 227)
     scripted_model = load_pretrained_model()
     mod, params  = import_pytorch_to_relay(scripted_model, input_name="input")
     tvm_lib = build_mnv2_lib(mod, params)
+    # tvm_lib.export_library("alexnet_tvm_lib.so")
+    # tvm_lib = tvm.runtime.load_module("/home/work/tvm_project_course/byoc/alexnet_tvm_lib.so")
     t0 = time.time()
     ncnn_lib = build_mnv2_lib(mod, params, codegen="ncnn")
+    ncnn_modules = extract_byoc_modules(ncnn_lib)
+
+    dump_module_to_json(ncnn_modules) 
+
     t1 = time.time()
     print("Time for build runtime library using ncnn codegen for mnv2 is:", t1-t0)
     tvm_output = run(tvm_lib, numpy_input_tensor)
     ncnn_output = run(ncnn_lib, numpy_input_tensor, codegen="ncnn")
-    print("tvm output is...\n", tvm_output.numpy()[0, :10])
-    print("ncnn output is...\n", ncnn_output.numpy()[0, :10])
+    print("tvm output is...\n", tvm_output.numpy()[0, :5])
+    print("ncnn output is...\n", ncnn_output.numpy()[0, :5])
